9c08b23bf3f61a77b50d306dc968a02850ad4ee2
RGero215 <rgero215@gmail.com>
2017-11-26 04:34:27 -0500

    moved dir

--- djangoEnv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/RGero13/Desktop/rgero215_PY1-10-2017/Belt_Reviewer/djangoEnv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
